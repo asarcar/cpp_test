752fec120ec3f56741d060b6cde60abe32bf8873
Arijit Sarcar <sarcar_a@yahoo.com>
2014-09-27 01:12:12 -0700

    Validated the use of vtables and std::function that captures function pointers, lambdas (closures), and functors

--- misc_test/src/experiment_test/fn_test.cc
+++ misc_test/src/experiment_test/fn_test.cc
@@ -10,6 +10,8 @@ struct TwoVal {
   TwoVal(const TwoVal& v) : i{v.i}, j{v.j} {}
 };
 
+const static TwoVal gkBase{20,30};
+
 namespace std {
 template <>
 struct greater<TwoVal> {
@@ -32,37 +34,75 @@ class Cmp {
   T base_;
 };
 
+bool IsGreater(const TwoVal& val) {
+  return greater<TwoVal>{}(val, gkBase);
+}
+
 int main(void) {
   Cmp<int>    fp{10};
-  Cmp<TwoVal> fc{TwoVal{20,30}};
+  Cmp<TwoVal> fc{gkBase};
 
-  function<bool(const int&)> fp2 = fp;
-  function<bool(const TwoVal&)> fc2 = fc;
+  /**
+   * MORAL:
+   * Function is a holder for any kind of function, 
+   * function object or closure of an appropriate type. 
+   * The "type" of function appropriate for us is something 
+   * taking const int& or const TwoVal& and returning a bool:
+   * e.g. std::function<bool(const TwoVal&)>
+   * We can assign to it any function pointer or function object 
+   * with a matching function signature:
+   * Not only is predicate able to "hold" any function-like 
+   * entity (of an appropriate function signature), but is also 
+   * a value: it can be copied or assigned to. 
+   * As is illustrated, it can be passed by value with no risk of 
+   * slicing - we added extra field (base_) in this case   
+   * because it guarantees to make a deep copy of the underlying 
+   * concrete object. We use it just as any other function...
+   */
 
   string s1("fp{10}");
   cout << s1 << endl;
   cout << string(55, '-') << endl;
   cout << s1 << left << setw(40) << " < fp(11)  " << boolalpha << fp(11) << endl;
+
+  function<bool(const int&)> fp2 = fp;
+  cout << "fp2: function<bool(const int&)> fp2 = fp" << endl;
   cout << s1 << left << setw(40) << " < fp2(09) " << boolalpha << fp2(9) << endl;
   cout << s1 << left << setw(40) << " < fp2(10) " << boolalpha << fp2(10) << endl;
   cout << s1 << left << setw(40) << " < fp2(07) " << boolalpha << fp2(7) << endl;
   cout << string(55, '=') << endl;
 
-  string s2("fc{TwoVal{20,30}}");
-  cout << "s2" << endl;
+  string s2("fc{gkBase}");
+  cout << "gkBase = TwoVal{20,30} = " << s2 << endl;
   cout << string(55, '-') << endl;
   cout << s2 << left << setw(29) << " < fc(TwoVal{10,40}))  " 
        << boolalpha << fc({10,40}) << endl;
+
+  function<bool(const TwoVal&)> fc2 = fc;
+  cout << "fc2: function<bool(const TwoVal&)> fc2 = fc" << endl;
   cout << s2 << left << setw(29) << " < fc2(TwoVal{25,25})) " 
        << boolalpha << fc2({25,25}) << endl;
   cout << s2 << left << setw(29) << " < fc2(TwoVal{20,40})) " 
        << boolalpha << fc2({20,40}) << endl;
-  cout << s2 << left << setw(29) << " < fc2(TwoVal{20,25})) " 
-       << boolalpha << fc2({20,25}) << endl;
-  cout << s2 << left << setw(29) << " < fc2(TwoVal{20,30})) " 
-       << boolalpha << fc2({20,30}) << endl;
-  cout << s2 << left << setw(29) << " < fc2(TwoVal{22, 28}) " 
-       << boolalpha << fc2(TwoVal{22,28}) << endl;
+  
+  function<bool(const TwoVal&)> fc3 = &IsGreater;
+  cout << "fc3: function<bool(const TwoVal&)> fc3 = &IsGreater" << endl;
+  cout << "     bool IsGreater(const TwoVal& val) {" << endl
+       << "       return greater<TwoVal>{}(val, gkBase);" << endl
+       << "     }" << endl;
+  cout << s2 << left << setw(29) << " < fc3(TwoVal{20,25})) " 
+       << boolalpha << fc3({20,25}) << endl;
+  
+  function<bool(const TwoVal&)> fc4 = [](const TwoVal &val) {
+    return greater<TwoVal>{}(val, gkBase);
+  };
+  cout << "fc4: function<bool(const TwoVal&)> fc3 = [](const TwoVal &val) {" << endl
+       << "       return greater<TwoVal>{}(val, gkBase);" << endl
+       << "     };" << endl;
+  cout << s2 << left << setw(29) << " < fc4(TwoVal{20,30})) " 
+       << boolalpha << fc4({20,30}) << endl;
+  cout << s2 << left << setw(29) << " < fc4(TwoVal{22, 28}) " 
+       << boolalpha << fc4(TwoVal{22,28}) << endl;
   cout << string(55, '=') << endl;
 
   return 0;
---
+++ misc_test/src/experiment_test/poly_test.cc
@@ -0,0 +1,91 @@
+#include <iomanip>     // std::left, std::setw ...
+#include <iostream>    // std::cout ...
+
+using namespace std;
+
+class Animal {
+ public:
+  virtual string eat(void) {return string("food");}
+ private:
+  int age = 0;
+};
+
+enum class eVegDiet {GRASS, LEAVES}; 
+enum class eNonVegDiet {FISH, DEER};
+
+class Herbivore: public Animal {
+ public:
+  string eat(void) override {return string("plant");}
+  virtual string call(void) {return string("sweet");}
+ private:
+  eVegDiet diet = eVegDiet::GRASS;
+};
+
+class Carnivore: public Animal {
+ public:
+  string eat(void) override {return string("meat");}
+  virtual string call(void) {return string("scary");}
+ private:
+  eNonVegDiet diet = eNonVegDiet::FISH;
+};
+
+class Cat: public Carnivore {
+ public:
+  string call(void) override {return string("meow");}
+ private:
+  bool dangerous = false;
+};
+
+class Tiger: public Carnivore {
+ public:
+  string call(void) override {return string("growl");}
+ private:
+  bool dangerous = true;
+};
+
+int main(void) {
+  Animal     a;
+  Herbivore  h;
+  Carnivore  c;
+  Cat        cat;
+  Tiger      tiger;
+  
+  Animal     *ah_p     = new Herbivore;
+  Animal     *ac_p     = new Carnivore;
+  Animal     *acat_p   = new Cat;
+  Animal     *atiger_p = new Tiger;
+
+  Carnivore  *ccat_p   = new Cat;
+  Carnivore  *ctiger_p = new Tiger;
+
+  /**
+   * MORAL:
+   * The new virtual methods added by children and grandchildren 
+   * can be handled by extending the vtable format from one base class. 
+   * Note: vtables used by multiple parents (inheritances) cannot be combined.
+   */
+  cout << "Sizes:" << endl;
+  cout << string(16, '-') << endl;
+  cout << left << setw(16) << "Animal "     << sizeof(a)     << endl;
+  cout << left << setw(16) << "Herbivore "  << sizeof(h)     << endl;
+  cout << left << setw(16) << "Carnivore "  << sizeof(c)     << endl;
+  cout << left << setw(16) << "Cat "        << sizeof(cat)   << endl;
+  cout << left << setw(16) << "Tiger "      << sizeof(tiger) << endl;
+  cout << endl;
+
+  cout << "Eat: " << endl;
+  cout << string(16, '-') << endl;
+  cout << left << setw(24) << "AnimalHerbivore " << ah_p->eat() << endl;
+  cout << left << setw(24) << "AnimalCarnivore " << ac_p->eat() << endl;
+  cout << left << setw(24) << "AnimalCat "       << acat_p->eat() << endl;
+  cout << left << setw(24) << "AnimalTiger "     << atiger_p->eat() << endl;
+  cout << endl;
+
+  cout << "Call: " << endl;
+  cout << string(16, '-') << endl;
+  cout << left << setw(24) << "CarnivoreCat "   << ccat_p->call() << endl;
+  cout << left << setw(24) << "CarnivoreTiger " << ctiger_p->call() << endl;
+  cout << endl;
+
+  return 0;
+}
