fc46123ef3b41ff280fcb4dada487b5469e1717c
Arijit Sarcar <sarcar_a@yahoo.com>
2014-12-04 23:30:12 -0800

    iptables.sh: added sample rules seen when running NAT e.g. between docker and external world; netns.sh: added rules to test VRF commands

--- src/experiment_test/ns_test/netns.sh
+++ src/experiment_test/ns_test/netns.sh
@@ -1,68 +1,51 @@
 #!/bin/sh
 
-echo ">> execute 'echo password | sudo -S ls' so that subsequent sudo commands do not require password"
-echo ">> close the terminal afterwards since we now have unrestricted sudo access"
-
+# 
+# * Create NS red, execute script inside red, and delete NS
+# > sudo ip netns add test && \
+#   sudo ip netns exec test sh netns.sh && \
+#   sudo ip netns del test
 #
-# Network Namespaces Basics
-#
-# Add Network Namespace red
-sudo ip netns add red
 
 # Move to or Execute cmd in 'red' NS bash session 
-sudo ip netns exec red bash -c "echo 'Execute cmd in NS Red!'"
-
-sudo ip netns exec red ip link list
+ip link list
 
 # Bring up the loopback interface of red NS
-sudo ip netns exec red ip link set dev lo up
-sudo ip netns exec red ping -c1 127.0.0.1
+ip link set dev lo up
+ping -c1 127.0.0.1
 
 # Create a "paired" virtual interface
-sudo ip link add veth0 type veth peer name veth1
-sudo ip link set veth0 netns red
-sudo ip link set veth1 netns red
+ip link add veth-test type veth peer name veth-base
+ip link set veth-base netns 1
 
 # Assign IP addresses to each pair
-sudo ip netns exec red ifconfig veth0 10.1.1.30/24 up
-sudo ip netns exec red ifconfig veth1 10.1.1.31/24 up
-sudo ip netns exec red ip link
-sudo ip netns exec red ifconfig
-
-sudo ip netns exec red ping -c1 10.1.1.31
-sudo ip netns exec red ping -c1 10.1.1.30
-
-sudo ip netns exec red route
-sudo ip netns exec red iptables -L
-
-# move veth1 from red NS to base using init PID=1
-sudo ip netns exec red ip link set veth1 netns 1
+ifconfig veth-test 10.1.1.31/24 up
+ping -c1 10.1.1.31
 
 # VLAN interface: parent interface may be in another namespace
-sudo ip link add link veth1 name veth1.100 type vlan id 100
-sudo ip link set veth1.100 netns red
+ip link add link veth-test name veth-test.100 type vlan id 100
 
 #
 # Use bridge commands
 # 
-sudo ip netns exec red brctl addbr br-red
-sudo ip netns exec red brctl addif br-red veth0
-sudo ip netns exec red brctl show
-sudo ip netns exec red brctl showmacs br-red
+brctl addbr br-test
+brctl addif br-test veth-test.100
+brctl show
+brctl showmacs br-test
 
 #
 # Adding new Tap/Tun Interfaces
 #
-sudo ip netns exec red ip tuntap add dev ethtap mode tap
-sudo ip netns exec red ip addr add 10.1.2.30/24 broadcast 10.1.2.255 dev ethtap
-sudo ip netns exec red ip link set dev ethtap up
-sudo ip netns exec red ping -c1 10.1.2.30
+ip tuntap add dev ethtap mode tap
+ip addr add 10.1.2.30/24 broadcast 10.1.2.255 dev ethtap
+ip link set dev ethtap up
+ping -c1 10.1.2.30
 
 #
 # Add multiple addresses to the same device
 #
-sudo ip netns exec red ip addr add 10.1.3.30/24 dev ethtap label ethtap:1
-sudo ip netns exec red ping -c1 10.1.3.30
+ip addr add 10.1.3.30/24 dev ethtap label ethtap:1
+ping -c1 10.1.3.30
 
 #
 # SIDE NOTE:
@@ -83,5 +66,26 @@ sudo ip netns exec red ping -c1 10.1.3.30
 # ebtables: netfilter
 #
 
-# Cleanup all state created via NS
-sudo ip netns del red 
+##################
+# VRF-lite test: #
+##################
+ip netns add test2
+ip link add veth-t1 type veth peer name veth-t2
+ip link set veth-t2 netns test2
+ip link set veth-t1 up
+ip netns exec test2 ip link set veth-t2 up
+ip addr add 10.1.4.31/24 dev veth-t1
+ip netns exec test2 ip addr add 10.1.4.32/24 dev veth-t2
+ip netns exec test2 ping -c1 10.1.4.31
+# Add VRF: append /etc/iproute2/rt_tables and add the VRF tables
+echo "100      TEST" >> /etc/iproute2/rt_tables
+ip netns exec test2 ip route add default via 10.1.4.31 dev veth-t2 proto static scope global table TEST
+# Create an ip rule list so that all lookups are executed in the RED VRF
+ip netns exec test2 ip rule add from default pref 100 lookup TEST
+# We are all set to execute a ping test to an interface and prefix 
+# outside the test2 NS
+ping -c1 10.1.2.30
+# Restore the rt_tables to original state
+sed -i '$ d' /etc/iproute2/rt_tables
+##################
+ip netns del test2
